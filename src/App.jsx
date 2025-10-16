ac378042bf92dad5baacddaa4855f76dd4aa4641
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-10-15 22:33:45 -0400

    completed step 4 of the workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { usePage } from "./layout/PageContext";
-
 import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
+import Layout from "./layout/Layout.jsx";
 import Error404 from "./Error404.jsx";
+import { Route, Routes } from "react-router";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -11,11 +11,14 @@ import Error404 from "./Error404.jsx";
  * account will be able to upload and manage their own activities.
  */
 export default function App() {
-  const { page } = usePage();
-
-  if (page === "register") return <Register />;
-  if (page === "login") return <Login />;
-  if (page === "activities") return <ActivitiesPage />;
-
-  return <Error404 />;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<ActivitiesPage></ActivitiesPage>}></Route>
+        <Route path="/Register" element={<Register></Register>}></Route>
+        <Route path="/Login" element={<Login></Login>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Route>
+    </Routes>
+  );
 }
